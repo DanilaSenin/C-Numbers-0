6b8309bc83b7730978dca28a57641c9900203d3b
Danila Senin <16502711@mail.ru>
2023-10-09 20:29:25 +0300

    Первое задание

---
+++ Number 1/Program.cs
@@ -0,0 +1,14 @@
+﻿bool Flag = false;
+while (Flag != true) {
+Console.WriteLine("Введите 2 числа");
+int firthN = int.Parse(Console.ReadLine());
+int ThotN = int.Parse(Console.ReadLine());
+
+if (firthN*firthN == ThotN) {
+    Flag = true;
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет, повторите попытку");
+}
+}
