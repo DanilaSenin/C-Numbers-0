8dfc1d8ff6781d3e9a2012e316b2cf46c7ba644d
Danila Senin <16502711@mail.ru>
2023-10-11 19:42:59 +0300

    Начало домашки

---
+++ Domachka/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = -99999999;
+int min = 99999999;
+if (a > b) {
+    max = a;
+    min = b;
+}
+else {
+    max = b;
+    min = a;
+}
+Console.WriteLine("a = " + a + " " + "b = " + b + " " + "->" + " max = " + max + " " + "min = "+ min);
---
+++ numder 4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое число");
+double a = int.Parse(Console.ReadLine());
+
+if (a != 0) {
+double b = 1 / a;
+//Console.SetCursorPosition(2, 7);
+Console.WriteLine("Обратное число = " + b);
+}
+else {
+    //Console.SetCursorPosition(7, 2);
+    Console.WriteLine("Обратное число от 0 = " + a);
+}
